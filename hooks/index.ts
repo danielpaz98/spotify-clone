5450e32a107f9b5640d307a33ddac6c3af08132d
Daniel Alexander Paz <alexdaniel-1998@outlook.com>
2024-04-20 18:30:25 -0500

    feat: add barrel for hooks

---
+++ hooks/index.ts
@@ -0,0 +1,7 @@
+export { default as useGetLikedSongs } from "./useGetLikedSongs";
+export { default as useGetSongById } from "./useGetSongById";
+export { default as useGetUserSongs } from "./useGetUserSongs";
+export { default as useLoadImageSong } from "./useLoadImageSong";
+export { default as useLoadSongUrl } from "./useLoadSongUrl";
+export { default as useOnPlay } from "./useOnPlay";
+export { default as usePlayer } from "./usePlayer";
