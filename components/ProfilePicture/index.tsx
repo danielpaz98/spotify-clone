91e1c01e1e87823e9d5e9f94732c633de1e3af36
Daniel Alexander Paz <alexdaniel-1998@outlook.com>
2024-04-20 18:34:48 -0500

    feat: add ProfilePicture component

---
+++ components/ProfilePicture/index.tsx
@@ -0,0 +1,47 @@
+// PLUGINS
+import Image from "next/image";
+import uniqolor from "uniqolor";
+// UTILS
+import { cn } from "@/utils";
+// MODELS
+import type { User } from "@/models";
+
+interface Props extends React.ComponentProps<"span"> {
+  user: User | null;
+}
+
+export default function ProfilePicture({ className, style, user, ...restProps }: Props) {
+  const { id: userId, full_name: username, avatar_url: image } = user || {};
+
+  if (image) {
+    return (
+      <figure className="w-6 h-6" title={username}>
+        <picture className="relative block w-full h-full rounded-full overflow-hidden">
+          <Image
+            fill
+            priority
+            alt={username || ""}
+            className="object-cover object-center"
+            sizes="32px"
+            src={image}
+          />
+        </picture>
+      </figure>
+    );
+  }
+
+  const firstLetter = username?.charAt(0);
+  const usernameColor = userId ? uniqolor(userId, { format: "hex", lightness: 50 }).color : "";
+  const classNames = cn("text-center text-black text-sm w-6 h-6 leading-6 rounded-full font-bold", className);
+
+  return (
+    <span
+      className={classNames}
+      style={{ ...style, backgroundColor: usernameColor }}
+      title={username}
+      {...restProps}
+    >
+      {firstLetter?.toUpperCase()}
+    </span>
+  );
+}
