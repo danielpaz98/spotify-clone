2dced6e3980bb60e5d40eca558965720ef7b1c37
Daniel Alexander Paz <alexdaniel-1998@outlook.com>
2024-04-20 18:36:40 -0500

    feat: add Sidebar component

---
+++ components/Sidebar/data.ts
@@ -0,0 +1,20 @@
+// ICONS
+import HomeIcon from "@/icons/home.svg";
+import HomeIcon2 from "@/icons/home-2.svg";
+import SearchIcon from "@/icons/search.svg";
+import SearchIcon2 from "@/icons/search-2.svg";
+
+export const sidebarMenu = [
+  {
+    href: "/",
+    text: "Home",
+    icon: HomeIcon,
+    icon2: HomeIcon2,
+  },
+  {
+    href: "/search",
+    text: "Search",
+    icon: SearchIcon,
+    icon2: SearchIcon2,
+  },
+];
---
+++ components/Sidebar/index.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { createContext, useState } from "react";
+// UTILS
+import { cn } from "@/utils";
+// MODELS
+import type { LibraryData } from "@/models";
+// COMPONENTS
+import { Box } from "@/components";
+import SideMenu from "./SideMenu";
+import Library from "./Library";
+// HOOKS
+import { useMainLayoutContext } from "@/layouts/MainLayout/hooks";
+// DATA
+import { sidebarMenu } from "./data";
+
+interface Props {
+  className?: string;
+  isOpen?: boolean;
+  libraryData: LibraryData | null;
+}
+
+const useValueContext = () => {
+  const [expanded, setExpaded] = useState(true);
+
+  return { expanded, setExpaded };
+};
+
+export const SidebarContext = createContext({} as ReturnType<typeof useValueContext>);
+
+export default function Sidebar({ className, libraryData }: Props) {
+  const value = useValueContext();
+  const { sidebarIsOpen } = useMainLayoutContext();
+
+  const classNames = cn(
+    "min-h-0 h-full w-[--left-sidebar-width] max-md:fixed max-md:z-50 max-md:transition-transform",
+    { "-translate-x-[100vw] md:translate-x-0": !sidebarIsOpen },
+    { "w-[72px]": !value.expanded },
+    className,
+  );
+
+  const navClassNames = /* tw */ cn(
+    "flex flex-col min-h-0 h-full divide-y divide-nero md:divide-y-0 md:gap-2",
+  );
+
+  return (
+    <SidebarContext.Provider value={value}>
+      <div className={classNames}>
+        <nav className={navClassNames}>
+          <Box className="rounded-none md:rounded-lg">
+            <SideMenu>
+              {sidebarMenu.map(({ text, href, icon, icon2 }) => (
+                <SideMenu.Item key={href} activeIcon={icon2} aria-label={text} href={href} icon={icon}>
+                  {value.expanded && <span>{text}</span>}
+                </SideMenu.Item>
+              ))}
+            </SideMenu>
+          </Box>
+
+          <Box className="flex flex-col flex-1 min-h-0 overflow-hidden rounded-none md:rounded-lg">
+            <Library libraryData={libraryData} tabIndex={-1} />
+          </Box>
+        </nav>
+      </div>
+    </SidebarContext.Provider>
+  );
+}
